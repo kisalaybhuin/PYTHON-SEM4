3a0dd3f0f1dce1e1c23cfc6ee3a7befe53c96730
Kisalay Bhuin <135982503+kisalaybhuin@users.noreply.github.com>
2025-01-03 23:17:53 +0530

    Add files via upload

---
+++ pg2_userchooseoperator.py
@@ -0,0 +1,12 @@
+a=int(input("Enter 1st value:"))
+b=int(input("Enter 2nd value:"))
+op=input("Enter Arithmetic operator:")
+if op=='+':
+    print(" Addition is:",a+b)
+if op=='-':
+    print(" Subtraction is:",a-b)
+if op=='*':
+    print(" Multiplication is:",a*b)
+if op=='/':
+    print(" Division is:",a/b)
+ 
