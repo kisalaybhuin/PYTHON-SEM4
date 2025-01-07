b5f6f635d8525d80c808152efb39387db7911d05
Kisalay Bhuin <135982503+kisalaybhuin@users.noreply.github.com>
2025-01-07 10:55:34 +0530

    Add files via upload

---
+++ pg7_AgeOfPerson.py
@@ -0,0 +1,10 @@
+a=int(input("Enter your Age : "))
+if a<12:
+    print("You are Kid")
+elif a>13 and 17>a:
+    print("You are Teenager")
+elif a>18 and 60>a:
+    print("You are Adult")
+elif a>60:
+    print("You are Senior Citizen")
+    
