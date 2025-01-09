340d50be1d1072862c8dc49aa7731e473bd181f7
Kisalay Bhuin <135982503+kisalaybhuin@users.noreply.github.com>
2025-01-09 10:47:55 +0530

    Add files via upload

---
+++ pg12_PrimeNumber.py
@@ -0,0 +1,12 @@
+#Write a program to input a number and display whether number is prime or not.
+
+num=int(input("Enter a number : "))
+if num > 1:
+    for i in range(2, (num//2)+1):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
